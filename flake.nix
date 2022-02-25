043fde2392676dc2988e0c3d4266d33a99e8220e
John Bargman <darthpjb@gmail.com>
2022-02-25 18:48:35 +0000

    implemented basic flake passthrough for CI, fixing build completition errors in uplc2c flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  nixConfig.bash-prompt = "[uplc2c_parent:] ";
+  description = "Parent flake for subfolders";
+  inputs =
+  {
+    uplc2c.url = "path:./compiler";
+
+    #CI integration
+    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, uplc2c, flake-compat, flake-compat-ci }:
+  {
+    packages.x86_64-linux = let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+     architecture = pkgs.callPackage ./.architecture/default.nix {};
+     };
+
+     defaultPackage.x86_64-linux = uplc2c.defaultPackage.x86_64-linux;
+  };
+}
