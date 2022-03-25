49e7fcb1902d0f8a50a555663f965929b57327f6
John Bargman <darthpjb@gmail.com>
2022-03-25 15:20:55 +0000

    updated CI integration to reflect new flake-pure functionality

--- flake.nix
+++ flake.nix
@@ -4,25 +4,15 @@
   inputs =
   {
     uplc2c.url = "path:./compiler";
-
-    #CI integration
-    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
-  outputs = { self, nixpkgs, uplc2c, flake-compat, flake-compat-ci }:
+  outputs = { self, nixpkgs, uplc2c }:
   {
     packages.x86_64-linux = let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
-     architecture = pkgs.callPackage ./architecture/default.nix {};
+      architecture = pkgs.callPackage ./architecture/default.nix {};
      };
 
-     ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
-           flake = self;
-           systems = [ "x86_64-linux" ];
-         };
+     herculesCI.ciSystems = [ "x86_64-linux" ];
      defaultPackage.x86_64-linux = uplc2c.defaultPackage.x86_64-linux;
   };
 }
