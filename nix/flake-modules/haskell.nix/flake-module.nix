e9f11fc4e186ba724826d8372c1f0dd265b5263a
matthewcroughan <matt@croughan.sh>
2022-06-01 22:57:30 +0100

    treewide: cleanup

---
+++ nix/flake-modules/haskell.nix/flake-module.nix
@@ -0,0 +1,28 @@
+{ lib, self, ... }:
+let
+  inherit (lib)
+    mkOption
+    types
+    ;
+in
+{
+  perSystem = { config, system, self', inputs', ... }: {
+    options = {
+      haskell-nix = {
+        pkgs = mkOption {
+          type = types.uniq (types.attrsOf types.unspecified);
+          description = ''
+            Nixpkgs to use for haskell-nix invocations. This is more of a
+            fixed-point that can be referred to in order to ensure we import
+            haskell-nix only once, for performance, since we haven't implemented
+            the rest of the module.
+          '';
+          default = import self.inputs.haskell-nix.inputs.nixpkgs {
+            inherit system;
+            overlays = [ self.inputs.haskell-nix.overlay ];
+          };
+        };
+      };
+    };
+  };
+}
