5ac4298909fb2b5c796d851a75652f0df46be5d2
Marcin Bugaj <marcin@mlabs.city>
2022-06-03 17:22:13 +0200

    Standard C headers custom impl in tree

---
+++ rts/tinyRAM_include/stddef.h
@@ -0,0 +1,10 @@
+#ifndef _STDDEF_H
+#define _STDDEF_H	1
+
+#include <stdint.h>
+
+#define NULL 0
+
+typedef int ptrdiff_t;
+
+#endif
---
+++ rts/tinyRAM_include/string.h
@@ -0,0 +1,8 @@
+#ifndef _STRING_H
+#define _STRING_H	1
+
+void *memcpy(void *dest, const void *src, unsigned int n);
+
+int memcmp(const void *ptr1, const void *ptr2, size_t num);
+
+#endif
