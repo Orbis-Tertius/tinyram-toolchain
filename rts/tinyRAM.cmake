6a8bbc7d531a35287516d049c2f36683d00342c7
Marcin Bugaj <marcin@mlabs.city>
2022-06-09 11:23:17 +0200

    Toolchain file cleanup

--- rts/tinyRAM.cmake
+++ rts/tinyRAM.cmake
@@ -1,5 +1,4 @@
 set(CMAKE_SYSTEM_NAME               Generic)
-set(CMAKE_SYSTEM_PROCESSOR          tinyRAM)
 
 # Without that flag CMake is not able to pass test compilation check
 set(CMAKE_TRY_COMPILE_TARGET_TYPE   STATIC_LIBRARY)
@@ -12,18 +11,14 @@ set(CMAKE_C_COMPILER                ${TINYRAM_TOOLCHAIN_PATH}clang)
 set(CMAKE_CXX_COMPILER              ${TINYRAM_TOOLCHAIN_PATH}clang++)
 
 set(CMAKE_CUSTOM_LINKER             ${TINYRAM_TOOLCHAIN_PATH}ld.lld)
-set(CMAKE_LINKER                    ${TINYRAM_TOOLCHAIN_PATH}ld.lld)
 set(CMAKE_C_LINK_EXECUTABLE
-    "/home/marcin/projects/llvm-project/build/bin/ld.lld --entry main <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+	"${CMAKE_CUSTOM_LINKER} --entry main <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 set(CMAKE_CXX_LINK_EXECUTABLE
-    "/home/marcin/projects/llvm-project/build/bin/ld.lld --entry main <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+	"${CMAKE_CUSTOM_LINKER} --entry main <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 set(CMAKE_OBJCOPY                   ${TINYRAM_TOOLCHAIN_PATH}llvm-objcopy)
 set(CMAKE_RANLIB                    ${TINYRAM_TOOLCHAIN_PATH}llvm-ranlib)
 set(CMAKE_SIZE                      ${TINYRAM_TOOLCHAIN_PATH}llvm-size)
 set(CMAKE_STRIP                     ${TINYRAM_TOOLCHAIN_PATH}llvm-strip)
-
-#set(CMAKE_C_FLAGS                   "-Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections" CACHE INTERNAL "")
-#set(CMAKE_CXX_FLAGS                 "${CMAKE_C_FLAGS} -fno-exceptions" CACHE INTERNAL "")
 
 set(CMAKE_C_FLAGS                   "-target tinyRAM -std=c11 -mllvm -enable-tail-merge=false")
 set(CMAKE_CXX_FLAGS                 "-target tinyRAM -mllvm -enable-tail-merge=false")
